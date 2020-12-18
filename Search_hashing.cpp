7dce4a228de2afa864c8da20e44340b705faf989
dannhh <70143744+dannhh@users.noreply.github.com>
2020-12-19 00:44:32 +0700

    Update and rename search.cpp to Search_hashing.cpp

--- Search_hashing.cpp
+++ Search_hashing.cpp
@@ -24,79 +24,6 @@ int jumpSearch(int arr[], int x, int n) {
    }
    return -1;
 }
-
-int foldShift(long long key, int addressSize)
-{
-    long long n = key;
-    int num = 0;
-    while(n != 0){
-        n = n/10;
-        num += 1;
-    }
-    int m = pow(10, num%addressSize);
-    long long result = key%m;
-    int size = pow(10, addressSize);
-    key = key/m;
-    while(key!=0){
-        result += key%size;
-        key = key/size;
-    }
-    if(result >= size) {
-        result = result%size;
-    }
-    return result;
-}
-
-int rotation(long long key, int addressSize)
-{
-    long long result = key%10;
-    key = key/10;
-    int size = 0;
-    long long n = key;
-    while(n!=0){
-        n/=10;
-        size++;
-    }
-    result = result*pow(10, size) + key;
-    long long k = foldShift(result, addressSize);
-        
-    return k;
-}
-
-long int midSquare(long int seed)
-{
-    long int seed_2 = seed*seed;
-    seed_2 = seed_2/100;
-    return seed_2%10000;
-}
-long int moduloDivision(long int seed, long int mod)
-{
-    return seed % mod;
-}
-#include <sstream>
-long int digitExtraction(long int seed,int* extractDigits,int size)
-{
-    int result = 0;
-    std::string s;
-    std::stringstream out;
-    out << seed;
-    s = out.str();
-
-    int i = 0;
-    while(i < size){
-        result = result*10+(s[extractDigits[i]]-48);
-        i++;
-    }
-    return result;
-    // string str = to_string(seed);
-    // string res = "";
-    // for (int i = 0; i < size; i++) {
-    //     res += str[extractDigits[i]];
-    // }
-    // int result = atoi(res.c_str());
-    // return result;
-}
-
 #include <algorithm>
 bool findPairs(int arr[], int n, pair<int,int>& pair1, pair<int, int>& pair2)
 {
@@ -146,8 +73,75 @@ int interpolationSearch(int arr[], int left, int right, int x)
     else if(arr[middle] < x) return interpolationSearch(arr, middle+1, right, x);
     else return interpolationSearch(arr, left, middle-1, x);
 }
-int main(){
- 	
+
+int foldShift(long long key, int addressSize)
+{
+    long long n = key;
+    int num = 0;
+    while(n != 0){
+        n = n/10;
+        num += 1;
+    }
+    int m = pow(10, num%addressSize);
+    long long result = key%m;
+    int size = pow(10, addressSize);
+    key = key/m;
+    while(key!=0){
+        result += key%size;
+        key = key/size;
+    }
+    if(result >= size) {
+        result = result%size;
+    }
+    return result;
+}
+
+int rotation(long long key, int addressSize)
+{
+    long long result = key%10;
+    key = key/10;
+    int size = 0;
+    long long n = key;
+    while(n!=0){
+        n/=10;
+        size++;
+    }
+    result = result*pow(10, size) + key;
+    long long k = foldShift(result, addressSize);
+        
+    return k;
+}
+
+long int midSquare(long int seed)
+{
+    long int seed_2 = seed*seed;
+    seed_2 = seed_2/100;
+    return seed_2%10000;
+}
+
+long int moduloDivision(long int seed, long int mod)
+{
+    return seed % mod;
+}
+
+#include <sstream>
+long int digitExtraction(long int seed,int* extractDigits,int size)
+{
+    int result = 0;
+    std::string s;
+    std::stringstream out;
+    out << seed;
+    s = out.str();
+
+    int i = 0;
+    while(i < size){
+        result = result*10+(s[extractDigits[i]]-48);
+        i++;
+    }
+    return result;
+}
+
+int main(){	
  	
 if(0){
     int arr[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 };
