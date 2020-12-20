47ab3cee6f4967302298db45911ba08d7213be73
dannhh <70143744+dannhh@users.noreply.github.com>
2020-12-20 11:12:34 +0700

    Update Kruskal.cpp

--- Graph/Kruskal.cpp
+++ Graph/Kruskal.cpp
@@ -15,12 +15,16 @@ struct Graph
         edges.push_back({ w, {u, v} });
     }
     
+    // Find set contain vertex i
     int findSet(vector<int>& store, int i){
         if(store[i] == i) return i;
         else
             return store[i] = findSet(store, store[i]);
     }
 
+    // Union 2 set into one
+	// Find set x of store contain i, set y of store contain j
+	// Rank: keep x "shorter" than y
     void unionSet(vector<int>& store, vector<int>& rank, int i, int j){
         int x = findSet(store, i);
         int y = findSet(store, j);
@@ -28,9 +32,12 @@ struct Graph
         store[x] = y;
         if(rank[x] == rank[y]) rank[y]++;
     }
-
+    
+    // Main function to get min weight of spanning trees
+    // How many spanning trees? (num of edge)C(num of vertexes-1) - num of cycles
     int kruskalMST() {
-		sort(edges.begin(), edges.end());
+        //Step 1: Sort edges's weight in non-decrease order
+	sort(edges.begin(), edges.end());		// O(ElogE)
         vector<int> store;
         store.assign(V, 0);
         for(int i = 0; i < V; i++)
@@ -39,11 +46,14 @@ struct Graph
         rank.assign(V, 0);
 		int mst_cost = 0, num_taken = 0;
         for(int i = 0; i < E; i++){
-            if(findSet(store, edges[i].second.first) == findSet(store, edges[i].second.second));
-            else{
-                mst_cost += edges[i].first;
+            if(findSet(store, edges[i].second.first) == findSet(store, edges[i].second.second))
+		// already in the same CC
+		    ;
+            else{	
+                mst_cost += edges[i].first;		// Update total weight
                 unionSet(store, rank, edges[i].second.first, edges[i].second.second);
                 ++num_taken;
+		//Spanning tree reach V-1 edges, finish
                 if(num_taken == V-1) break;
             }
         }
